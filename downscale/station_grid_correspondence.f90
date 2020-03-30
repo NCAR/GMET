1ca67635d18097c78a5e8172972eee4d541b735f
Andy Newman <anewman@ucar.edu>
2020-03-30 18:57:07 +0000

    Bug fixes for station-grid correspondence, allocations and proper removal of precipitation dynamic predictor

--- downscale/station_grid_correspondence.f90
+++ downscale/station_grid_correspondence.f90
@@ -1,7 +1,9 @@
-subroutine station_grid_correspondence(close_weights,close_weights_t,close_loc,close_loc_t,nSta,nearestGridpoint)
+subroutine station_grid_correspondence(X,Z,close_weights,close_weights_t,close_loc,close_loc_t,nSta,nearestGridpoint)
   use type
   implicit none
 
+  real(dp), intent(in)        :: X(:,:)
+  real(dp), intent(in)        :: Z(:,:)
   real(dp), intent(in)        :: close_weights(:,:)
   real(dp), intent(in)        :: close_weights_t(:,:)
   integer(I4B), intent(in)    :: close_loc(:,:) 
@@ -17,14 +19,18 @@ subroutine station_grid_correspondence(close_weights,close_weights_t,close_loc,c
   integer(I4B)                :: nGrid
   real(dp),allocatable        :: closestWeight(:)      ! weights for closest gridpoint to a station
 
+  integer(I4B)                :: tmp_grid_pt
+  real(dp)                    :: tmp_weight
+  real(dp)                    :: max_tmp_weight
+
   !code starts below
   vshape = shape(close_weights)
 
 
   allocate(closestWeight(nSta))
 
-  closestWeight(:) = 0.0
-
+  closestWeight = 0.0
+  max_tmp_weight = 0.0
 
   ngrid = vshape(1)
   nStaGrid = vshape(2)
@@ -40,7 +46,32 @@ subroutine station_grid_correspondence(close_weights,close_weights_t,close_loc,c
         nearestGridPoint(close_loc_t(g,s)) = g;
       end if
 
+      if(nearestGridPoint(close_loc(g,s)) .lt. 1) then
+        print *,'p',g,close_weights(g,:)
+      end if
+      if(nearestGridPoint(close_loc_t(g,s)) .lt. 1) then
+        print *,'t',g,close_weights_t(g,:)
+      end if
+    
     end do
   end do
 
+  !check for stations that do not have a grid correspondence yet
+  do s = 1, nSta, 1
+    if(nearestGridPoint(s) .eq. 0) then
+      do g = 1, ngrid, 1
+        ! x() are station lonlat; z() are grid lonlat; returns weight (w_base) for grd-to-stn
+        call calc_distance_weight(real(500.,dp), X(s,2), X(s,3), Z(g,2), Z(g,3), tmp_weight)      
+        if(tmp_weight > max_tmp_weight) then
+          max_tmp_weight = tmp_weight
+          tmp_grid_pt = g
+        end if
+      end do
+      nearestGridPoint(s) = tmp_grid_pt
+      !reset max_tmp_weight
+      max_tmp_weight = 0
+    end if
+  end do
+
+!  print *,nearestGridPoint
 end subroutine
